429a6fb87997b43b140e62bc0d91620a6eb680b9
Mike Gretton <mikegretton@gmail.com>
2020-05-08 02:57:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 function takeANumber(line, name) {
-  if (typeof line === 'undefined') {
+  if (line.length === 0) {
     line = [name];
+    return `Congrats, ${name}, you are number 1 in the line!`;
   } else {
     line.push(name);
+    return `Hello, ${name}, you are number ${line.length} in the line.`;
   }
 }
